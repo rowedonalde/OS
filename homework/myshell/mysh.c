452ce18f820261d21bf9b06eccbd3f8c5054ca5a
Don Rowe <rowedonalde@gmail.com>
2012-04-17 02:57:07 -0700

    Added beginning of code to handle "cd". However, I need to tokenize based on whitespace, not just line breaks (scanf, as I'm handling it, does this anyway, and execlp expects it).

--- homework/myshell/mysh.c
+++ homework/myshell/mysh.c
@@ -8,6 +8,9 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
+#define CHANGE_DIR_COMMAND "cd "
 
 /**
  * This program is a terminal shell for Linux
@@ -19,6 +22,9 @@ int main() {
     
     /* String to hold the command to run. */
     char command[256];
+    
+    /* String to hold the new directory */
+    char new_dir[253];
     
     /* The int that should be used to hold the status from the wait call */
     int result;
@@ -39,6 +45,34 @@ int main() {
             printf("\n");
             return 0;
         }
+        
+        /* 
+         * If the user types a command beginning with "cd" change to that
+         * directory. Do not fork a new process--just start the read loop over.
+         *
+         * TODO: allow whitespace before the cd command
+         */
+        if (strncmp(CHANGE_DIR_COMMAND, command, 3) == 0) {
+                //test:
+                printf("cd command detected!\n");
+            /* set new_dir to the stuff in command after "cd " */
+            /* since command is null-terminated, new_dir will be as well */
+            int i;
+            for (i = 0; i < 256 - 3; i++) {
+                new_dir[i] = command[i + 3];
+            }
+            
+            /* Change to new_dir */
+            if (chdir(new_dir) == 0) {
+                printf("New working directory: %s\n", new_dir);
+            } else {
+                printf("There was an error in changing the working directory!\n");
+            }
+            
+            pid = 1;
+            continue;
+        }
+          
 
         /* Perform the actual fork */
         pid = fork();
