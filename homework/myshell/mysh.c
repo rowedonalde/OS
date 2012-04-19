c3aa691d93e09f845477634327c369dcf9ce65ff
Don Rowe <rowedonalde@gmail.com>
2012-04-18 23:08:02 -0700

    mysh.c can now take multiple arguments. I had to backpedal on the EOF detection, though.

--- homework/myshell/mysh.c
+++ homework/myshell/mysh.c
@@ -9,8 +9,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define CHANGE_DIR_COMMAND "cd "
+#define CHANGE_DIR_COMMAND "cd"
 #define MAX_TOKEN_LENGTH 256
 
 /**
@@ -52,55 +53,42 @@ int main() {
         
         /* Get the command from the user */
         printf("Enter the command to run: ");
-        /* FIXME: This needs to be looped through to get successive tokens.
-           presently it only gets the first */
            
         /* Get the raw input*/
-        //scanf_code = scanf("%s", command);
         fgets(raw_input, MAX_TOKEN_LENGTH, stdin);
         
         /* If the user types ctrl-D, just quit */
-        if (strspn(eof_str, raw_input)) {
-            printf("\n");
-            return 0;
-        }
+//         if (strspn(eof_str, raw_input)) {
+//             printf("\n");
+//             return 0;
+//         }
         
         /* Get the command */
-        command = strtok(raw_input, " ");
+        args[0] = strtok(raw_input, " \n");
             
         /*Loop through and add arguments to the list */
-        while (args[args_len] = strtok(NULL, " ")) {
+        while (args[args_len + 1] = strtok(NULL, " \n")) {
             args_len++;
         }
         
-            //test:
-            printf("command: %s\n", command);
-            printf("args:\n");
-            for (i = 0; i < args_len; i++) {
-                printf("%s\n", args[i]);
-            }
+// test:
+//              printf("command: %s\n", command);
+//              printf("args:\n");
+//              for (i = 0; i < args_len; i++) {
+//                  printf("%s\n", args[i]);
+//              }
         
-//         /* 
-//          * If the user types a command beginning with "cd" change to that
-//          * directory. Do not fork a new process--just start the read loop over.
-//          *
-//          * TODO: allow whitespace before the cd command
-//          * FIXME: This is all wrong--everything gets tokenized by whitespace,
-//          * not just by line breaks.
-//          */
-//         if (strncmp(CHANGE_DIR_COMMAND, command, 3) == 0) {
+        /* 
+         * If the user types a command beginning with "cd" change to that
+         * directory. Do not fork a new process--just start the read loop over.
+         */
+//         if (strspn(CHANGE_DIR_COMMAND, command)) {
 //                 //test:
 //                 printf("cd command detected!\n");
-//             /* set new_dir to the stuff in command after "cd " */
-//             /* since command is null-terminated, new_dir will be as well */
-//             int i;
-//             for (i = 0; i < 256 - 3; i++) {
-//                 new_dir[i] = command[i + 3];
-//             }
 //             
-//             /* Change to new_dir */
-//             if (chdir(new_dir) == 0) {
-//                 printf("New working directory: %s\n", new_dir);
+//             /* Change to new dir */
+//             if (chdir(args[0]) == 0) {
+//                 printf("New working directory: %s\n", args[0]);
 //             } else {
 //                 printf("There was an error in changing the working directory!\n");
 //             }
@@ -108,24 +96,29 @@ int main() {
 //             pid = 1;
 //             continue;
 //         }
-//           
-// 
-//         /* Perform the actual fork */
-//         pid = fork();
-//         
-//         /* If this is the parent process, wait on the child */
-//         if (pid > 0) { 
-//             wait(&result);
-//         }
-//         
+          
+ 
+        /* Perform the actual fork */
+        pid = fork();
+            //test:
+            //printf("my pid is %d\n", pid);
+        
+        /* If this is the parent process, wait on the child */
+        if (pid > 0) { 
+            wait(&result);
+        }
+        
      } while (pid > 0);
     
     /* If this is the child process, run the command the user passed to it */
     if (pid == 0) {
-        /* FIXME: be able to handle an arbitrary number of args:
-           use execvp() instead */
-        execlp(command, command, NULL);
-   }
+        args[args_len + 1] = NULL;
+        if (args_len > 0) {
+            execvp(args[0], args);
+        } else {
+            execlp(args[0], args[0], NULL);
+        }
+    }
 
     return 0;
 }
