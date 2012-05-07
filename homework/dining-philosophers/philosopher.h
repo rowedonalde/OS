9775a899246779ddbdc6a47ca44df7d96ce23b95
don <drowe2@lion.lmu.edu>
2012-05-07 03:21:16 -0700

    Created header philosopher.h

---
+++ homework/dining-philosophers/philosopher.h
@@ -0,0 +1,13 @@
+/*
+ * philosopher.h
+ *
+ * Function for running a philosopher's wait-eat-think loop
+ */
+
+#ifndef __PHILOSOPHER__
+
+#define __PHILOSOPHER__
+
+void phloop(int phil, int total, float chance);
+
+#endif
