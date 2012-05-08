c29d005cfc6a9433c3993b47f8d2cd780b9ddec6
don <drowe2@lion.lmu.edu>
2012-05-07 20:29:14 -0700

    Added wait() and signal() declarations to choptick.c and chopstick.h

--- homework/dining-philosophers/chopstick.c
+++ homework/dining-philosophers/chopstick.c
@@ -1,3 +1,31 @@
 /*
  * chopstick.c
+ *
+ * Functions for philosophers to use in handling chopsticks
  */
+
+#include "chopstick.h"
+#define FREE -1
+
+
+/*
+ * wait() function used by a philosopher to wait until a given chopstick is
+ * ready, after which the chopstick is marked as being held by that philosopher
+ */
+void wait(int* chopstick, int phil) {
+    while (*chopstick > FREE) {
+        //wait
+    }
+    
+    //When done waiting, set the given chopstick to
+    //the philosopher's ID:
+    *chopstick = phil;
+}
+
+/*
+ * signal() function used by a philosopher to indicate that the given chopstick
+ * is now available.
+ */
+void signal(int* chopstick) {
+    *chopstick = FREE;
+}
--- homework/dining-philosophers/chopstick.h
+++ homework/dining-philosophers/chopstick.h
@@ -1,6 +1,11 @@
+/*
+ * chopstick.h
+ */
+
 #ifndef __CHOPSTICK__
 #define __CHOPSTICK__
 
-
+void wait(int* chopstick, int phil);
+void signal(int* chopstick);
 
 #endif
