452dfc3bb96d96c04ce59f6ae1ed7e55e4bf7ace
don <drowe2@lion.lmu.edu>
2012-05-09 00:10:42 -0700

    Began print_status() function in chopstick.c and added sync.h header reference

--- homework/dining-philosophers/chopstick.c
+++ homework/dining-philosophers/chopstick.c
@@ -1,10 +1,16 @@
 /*
  * chopstick.c
  *
- * Functions for philosophers to use in handling chopsticks
+ * Functions for philosophers to use in handling chopsticks.
+ * 
+ * This implementation assumes that a chopstick is to the left of
+ * the philosopher of the same ID number.
  */
 
 #include "chopstick.h"
+
+#include "sync.h"
+
 #define FREE -1
 
 
@@ -28,4 +34,32 @@ void wait(int* chopstick, int phil) {
  */
 void signal(int* chopstick) {
     *chopstick = FREE;
+}
+
+
+/*
+ * Prints out the current status of an array of chopsticks.
+ * If a chopstick is available, it is represented as a '|' character.
+ * If the philosopher to the left is holding it, it is represented as
+ * a '<' character.
+ * If the philosopher to the right is holding it, it is represented as
+ * a '>' character.
+ */
+void print_status(int* chopstick, int total, int* waitlist) {
+    char *errormsg =
+    "\n******ILLEGAL OPERATION! PHILOSOPHER %d IS HOLDING CHOPSTICK %d******\n";
+
+    //Chopstick 0 is an edge case--the philosopher to the left
+    //is philosopher total-1
+    if (total > 0) {
+        if (chopstick[0] == 0) {
+            printf(">0");
+        } else if (chopstick[0] == total - 1) {
+            printf("<0");
+        } else if (chopstick[0] == FREE) {
+            printf("|0");
+        } else {
+            printf(errormsg, chopstick[0], 0);
+        }
+    }
 }
