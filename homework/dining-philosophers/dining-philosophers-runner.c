58cb9fa98447d183e6a26c1435f7dcd75b3df037
don <drowe2@lion.lmu.edu>
2012-05-09 03:35:10 -0700

    Added explanatory comment to dining-philosophers-runner.c

--- homework/dining-philosophers/dining-philosophers-runner.c
+++ homework/dining-philosophers/dining-philosophers-runner.c
@@ -1,3 +1,13 @@
 /*
  * dining_philosophers_runner.c
+ *
+ * This program runs a Dining Philosophers simulation. It
+ * takes two arguments from the command line: an int n:n>1
+ * number of philosophers and an int t:t>0 second wait bound.
+ *
+ * This file defines a thread interface for the philosopher
+ * loop. The main() function first initializes the chopstick
+ * mutex array. Then, it initializes the threads that run
+ * each philosopher loop. This process will run indefinitely
+ * until it is killed.
  */
